20cdd34ee731fdd03eea15049cbd4a402cd31fd7
ChangeDL <douglas.loizzo@gmail.com>
2022-12-22 01:10:28 -0500

    Splash Page CSS completee. In dev links fix later

---
+++ react-app/src/components/WorkInProgress/WorkInProgress.js
@@ -0,0 +1,10 @@
+
+const WorkInProgress = () => {
+    return (
+        <>
+            <h1>Page is currently in development. Please come back later!</h1>
+        </>
+    )
+}
+
+export default WorkInProgress
