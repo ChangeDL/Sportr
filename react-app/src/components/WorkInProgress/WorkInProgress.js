0d3769962d1022fdc22aa7aed1c887df0c56f73e
ChangeDL <douglas.loizzo@gmail.com>
2022-12-22 11:55:56 -0500

    under dev page fixed to be under NavBar

--- react-app/src/components/WorkInProgress/WorkInProgress.js
+++ react-app/src/components/WorkInProgress/WorkInProgress.js
@@ -1,8 +1,12 @@
+import './WorkInProgress.css'
+
 
 const WorkInProgress = () => {
     return (
         <>
-            <h1>Page is currently in development. Please come back later!</h1>
+            <div className="work-progress-container">
+                <h1>Page is currently in development. Please come back later!</h1>
+            </div>
         </>
     )
 }
