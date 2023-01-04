063f5f8a36e5bfa88c380628ae5a575317744f05
ChangeDL <douglas.loizzo@gmail.com>
2023-01-04 13:33:06 -0500

    adjustment to join table

--- app/models/images.py
+++ app/models/images.py
@@ -7,7 +7,7 @@ image_album = db.Table(
     db.Column('album_id', db.Integer, db.ForeignKey(add_prefix_for_prod('albums.id')))
 )
 if environment == "production":
-    image_album = {'schema': SCHEMA}
+    image_album.schema =  SCHEMA
 
 
 class Image(db.Model):
