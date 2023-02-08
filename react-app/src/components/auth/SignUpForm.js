e7a62abd4bbaad0afd4120191d03036f467a1727
ChangeDL <douglas.loizzo@gmail.com>
2023-02-08 13:59:43 -0500

    sign up errors updated for where and when they're displayed and demo user button added to sign up form

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Link, Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
+import { signUp, login } from '../../store/session';
 import './SignUpForm.css'
 import Logo from '../../assets/misc/Logo.png'
 
@@ -9,6 +9,7 @@ const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
+  const [formErrors, setFormErrors] = useState({})
   const [username, setUsername] = useState('');
   const [full_name, setFullName] = useState('')
   const [email, setEmail] = useState('');
@@ -23,27 +24,30 @@ const SignUpForm = () => {
   }, [])
 
 
+  useEffect(() => {
+    if (errors.length > 0) setDisable(true)
+    if (errors.length === 0 && username.length > 1 && password.length > 1 && full_name.length > 1 && repeatPassword.length > 1) setDisable(false)
+  }, [errors, disable, username, password, repeatPassword, full_name])
 
 
 
   useEffect(() => {
-    const errors = []
-    if (username.length < 4) errors.push("Username must be at least 4 characters or more.")
-    if (username.length > 25) errors.push("Username is too long please make it less then 25 characters")
-    if (password.length < 8) errors.push("Password must be at least 8 characters or more.")
-    if (password !== repeatPassword) errors.push("Repeated Password doesn't match Password.")
-    if (full_name.length === 0 || !letters.includes(full_name[0])) errors.push('Please enter your full name')
-    if (full_name.length !== 0 && full_name.length < 4) errors.push('Full names but be at least 4 characters long')
-    if (full_name.length > 40) errors.push('Please enter your full name at a length less than 40 characters')
-    if (errors.length > 0) setDisable(true)
-    if (errors.length === 0) setDisable(false)
+    const errors = {}
+    if (username.length < 4 && username.length > 0) errors.username = ("Username must be at least 4 characters or more.")
+    if (username.length > 25) errors.username = ("Username is too long please make it less then 25 characters")
+    if (password.length < 8 && password.length > 0) errors.password = ("Password must be at least 8 characters or more.")
+    if (password !== repeatPassword && repeatPassword.length > 0) errors.repeatPassword = ("Repeated Password doesn't match Password.")
+    if (full_name.length > 0 && !letters.includes(full_name[0])) errors.fullName = ('Please enter your full name')
+    if (full_name.length !== 0 && full_name.length < 4) errors.fullName = ('Full names but be at least 4 characters long')
+    if (full_name.length > 40) errors.fullName = ('Please enter your full name at a length less than 40 characters')
+    if (Object.keys(errors).length > 0) setDisable(true)
+    if (Object.keys(errors).length === 0 && username.length > 1 && password.length > 1 && full_name.length > 1 && repeatPassword.length > 1) setDisable(false)
 
-    setErrors(errors)
+    setFormErrors(errors)
   }, [username, password, repeatPassword, full_name, disable])
 
   const onSignUp = async (e) => {
     e.preventDefault();
-    if (errors.length > 0) return setErrors([])
     if (password === repeatPassword) {
       const data = await dispatch(signUp(username.toLowerCase(), full_name, email.toLowerCase(), password));
       if (data) {
@@ -51,6 +55,15 @@ const SignUpForm = () => {
       }
     }
   };
+
+  const demoUser = async (e) => {
+    e.preventDefault()
+
+    const demoEmail = 'demo@aa.io'
+    const demoPassword = 'password'
+
+    return await dispatch(login(demoEmail, demoPassword))
+  }
 
   const updateUsername = (e) => {
     setUsername(e.target.value);
@@ -102,6 +115,9 @@ const SignUpForm = () => {
                 value={username}
                 required={true}
               ></input>
+              {formErrors.username ?
+                <div className='errors-for-sign-up'>{formErrors.username}</div>
+                : null}
             </div>
             <div className='all-sign-up-form-inputs-labels'>
               <input
@@ -113,6 +129,9 @@ const SignUpForm = () => {
                 value={full_name}
                 required={true}
               ></input>
+              {formErrors.fullName ?
+                <div className='errors-for-sign-up'>{formErrors.fullName}</div>
+                : null}
             </div>
             <div className='all-sign-up-form-inputs-labels'>
               <input
@@ -135,6 +154,9 @@ const SignUpForm = () => {
                 value={password}
                 required={true}
               ></input>
+              {formErrors.password ?
+                <div className='errors-for-sign-up'>{formErrors.password}</div>
+                : null}
             </div>
             <div className='all-sign-up-form-inputs-labels'>
               <input
@@ -146,9 +168,13 @@ const SignUpForm = () => {
                 value={repeatPassword}
                 required={true}
               ></input>
+              {formErrors.repeatPassword ?
+                <div className='errors-for-sign-up'>{formErrors.repeatPassword}</div>
+                : null}
             </div>
             <div className='sign-up-submit-button-div'>
               <button disabled={disable} className='sign-up-submit-button' type='submit'>Sign Up</button>
+              <button className='login-submit-button' onClick={event => demoUser(event)}>Demo User</button>
             </div>
             <div className='terms-of-service-sign-up-div'>
               <span className='terms-of-service-sign-up'>By signing up, you agree with Sportr's <Link to='/page-in-development' style={{ textDecoration: 'none', color: 'rgb(0,130,199)' }}>Terms of Services</Link> and <Link to='/page-in-development' style={{ textDecoration: 'none', color: 'rgb(0,130,199)' }}>Privacy Policy</Link></span>
